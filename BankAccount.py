bc0710e2f0b09e27962de2b668de9fbd24a3a094
soyoung <jsy03070@naver.com>
2023-12-12 15:18:44 +0900

    .

--- BankAccount.py
+++ BankAccount.py
@@ -12,11 +12,13 @@ class BankAccount:
     def withdraw(self, amount):
         self.__balance -= amount
     def __str__(self):
-        return "{0} , {1} , {2}".format(self.id, \
-            self.name, self.balance)
+        return "{0} , {1} , {2}".format(self.__id, \
+            self.__name, self.__balance)
 
 #인스턴스 객체를 생성
 account1 = BankAccount(100, "전우치", 15000)
 account1.deposit(5000)
 account1.withdraw(3000)
 print(account1)
+
+
